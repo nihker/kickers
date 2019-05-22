be56ceb015d6e0683fcb9b79b252e42d4d7c6a18
nihker <nihadkeric@gmail.com>
2019-05-22 07:35:53 +0200

    Create card animation with Reveal Fade

--- src/Components/Home/meetPlayers/index.js
+++ src/Components/Home/meetPlayers/index.js
@@ -1,74 +1,89 @@
 import React, { Component } from 'react';
 import Stripes from '../../../Resources/images/stripes.png';
 import { Tag } from '../../ui/misc';
+import Fade from 'react-reveal/Fade';
+import HomeCards from './cards';
 
 class MeetPlayers extends Component {
 
     state = {
-
+        show: false
     }
 
     render() {
         return (
-            <div
-                className="home_meetplayers"
-                style={{ background: `#ffffff url(${Stripes})` }}
+            <Fade
+                fraction={0.4}
+                onReveal={
+                    () => {
+                        this.setState({
+                            show: true
+                        })
+                    }
+                }
             >
-                <div className="container">
-                    <div className="home_meetplayers_wrapper">
-                        <div className="home_card_wrapper">
-                            card
+                <div
+                    className="home_meetplayers"
+                    style={{ background: `#ffffff url(${Stripes})` }}
+                >
+                    <div className="container">
+                        <div className="home_meetplayers_wrapper">
+                            <div className="home_card_wrapper">
+                                <HomeCards
+                                    show={this.state.show}
+                                />
                         </div>
-                        <div className="home_text_wrapper">
-                            <Tag
-                                bck="#0e1732"
-                                size="100px"
-                                color="#ffffff"
-                                add={{
-                                    display: 'inline-block',
-                                    marginBottom: '20px',
-                                }}
-                            >
-                                meet
+                            <div className="home_text_wrapper">
+                                <Tag
+                                    bck="#0e1732"
+                                    size="100px"
+                                    color="#ffffff"
+                                    add={{
+                                        display: 'inline-block',
+                                        marginBottom: '20px',
+                                    }}
+                                >
+                                    meet
                             </Tag>
-                            <Tag
-                                bck="#0e1732"
-                                size="100px"
-                                color="#ffffff"
-                                add={{
-                                    display: 'inline-block',
-                                    marginBottom: '20px',
-                                }}
-                            >
-                                the
+                                <Tag
+                                    bck="#0e1732"
+                                    size="100px"
+                                    color="#ffffff"
+                                    add={{
+                                        display: 'inline-block',
+                                        marginBottom: '20px',
+                                    }}
+                                >
+                                    the
                             </Tag>
-                            <Tag
-                                bck="#0e1732"
-                                size="100px"
-                                color="#ffffff"
-                                add={{
-                                    display: 'inline-block',
-                                    marginBottom: '20px',
-                                }}
-                            >
-                                players
+                                <Tag
+                                    bck="#0e1732"
+                                    size="100px"
+                                    color="#ffffff"
+                                    add={{
+                                        display: 'inline-block',
+                                        marginBottom: '20px',
+                                    }}
+                                >
+                                    players
                             </Tag>
-                            <Tag
-                                bck="#ffff"
-                                size="27px"
-                                color="#0e1731"
-                                link={true}
-                                linkto="/the_team"
-                                add={{
-                                    display: 'inline-block',
-                                    marginBottom: '27px',
-                                    border:'1px solid #0e1731'
-                                }}
-                            >Meet them here</Tag>
+                                <Tag
+                                    bck="#ffff"
+                                    size="27px"
+                                    color="#0e1731"
+                                    link={true}
+                                    linkto="/the_team"
+                                    add={{
+                                        display: 'inline-block',
+                                        marginBottom: '27px',
+                                        border: '1px solid #0e1731'
+                                    }}
+                                >Meet them here</Tag>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Fade>
         );
     }
 }
