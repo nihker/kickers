32cb1575e73c35ed98c1ffa13b5f2841e470597c
nihker <nihadkeric@gmail.com>
2019-05-22 08:09:29 +0200

    Update last changes on Promotion Section

---
+++ src/Components/Home/promotion/Animation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const PromotionAnimation = () => {
+    return (
+        <div>
+            animation
+        </div>
+    )
+}
+
+export default PromotionAnimation
---
+++ src/Components/Home/promotion/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PromotioAnimation from './Animation'
+
+const Promotion = () => {
+    return (
+        <div className="promotion_wrapper" style={{background: '#ffffff'}}>
+            <div className="container">
+                <PromotioAnimation/>
+            </div>
+        </div>
+    )
+}
+
+export default Promotion
