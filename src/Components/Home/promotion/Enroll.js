5373d70508ecfd216a40e36097686296fb02019d
nihker <nihadkeric@gmail.com>
2019-05-23 15:21:18 +0200

    Handling errors and create submit func

--- src/Components/Home/promotion/Enroll.js
+++ src/Components/Home/promotion/Enroll.js
@@ -27,8 +27,25 @@ class Enroll extends Component {
         }
     }
 
-    submitForm() {
-        
+    submitForm(event) {
+        event.preventDefault();
+
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for(let key in this.state.formdata) {
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid;
+        }
+
+        if(formIsValid) {
+            
+        } else {
+            // have a error
+            this.setState({
+                formError: true
+            })
+        }
     }
 
     updateForm(element) {
@@ -49,7 +66,8 @@ class Enroll extends Component {
         newFormData[element.id] = newElement;
 
         this.setState({
-            formdata: newFormData
+            formdata: newFormData,
+            formError: false
         })
 
     }
@@ -68,6 +86,10 @@ class Enroll extends Component {
                                 formdata={this.state.formdata.email}
                                 change={(element) => this.updateForm(element)}
                             />
+                            { this.state.formError ? 
+                                <div className="error_label">Something is wrong, try again!</div> 
+                                : null}
+                            <button onClick={(event) => this.submitForm(event)}>Enroll</button>
                         </div>
                     </form>
                 </div>
