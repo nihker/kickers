f0398dc2f1b5e4c60f055afbfdf0a7b883609293
nihker <nihadkeric@gmail.com>
2019-05-23 07:57:34 +0200

    Validate email (regex)

--- src/Components/ui/misc.js
+++ src/Components/ui/misc.js
@@ -39,6 +39,13 @@ export const firebaseLooper = (snapshot) => {
 export const validate = (element) => {
     let error = [true,'']
 
+    if(element.validation.email){
+        var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i); 
+        const valid = emailReg.test(element.value);
+        const message = `${!valid ? 'Must be a valid email':''}`;
+        error = !valid ? [valid,message]: error;
+    }
+
     if(element.validation.required) {
         const valid = element.value.trim() !== '';
         const message = `${!valid ? 'This field is required':''}`;
