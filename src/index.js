13a2d1faaf9c476d85b0977a4ea8c41cf7e092e0
nihker <nihadkeric@gmail.com>
2019-05-28 07:18:04 +0200

    Create authRoute for private route

--- src/index.js
+++ src/index.js
@@ -6,16 +6,15 @@ import Routes from './routes';
 import './Resources/css/app.css';
 import { firebase } from './firebase';
 
-const App = () => {
+const App = (props) => {
     return (
         <BrowserRouter>
-            <Routes/>
+            <Routes {...props}/>
         </BrowserRouter>
     )
 }
 
 firebase.auth().onAuthStateChanged((user) => {
-    console.log(user)
     ReactDOM.render(<App user={user} />, document.getElementById('root'));
 })
 
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoutes from './Components/authRoutes/privateRoutes'
+
 import Home from './Components/Home';
 import SignIn from './Components/Signin';
 import Dashboard from './Components/Admin/Dashboard';
@@ -10,7 +12,10 @@ const Routes = (props) => {
   return (
    <Layout>
      <Switch>
-        <Route exact component={Dashboard} path="/dashboard" />
+
+        <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard}/>
+
+        {/* <Route exact component={Dashboard} path="/dashboard" /> */}
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
      </Switch>
