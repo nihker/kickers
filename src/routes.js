68fb55e2c9b4afd151d39401368bcd233030cad5
nihker <nihadkeric@gmail.com>
2019-05-28 08:47:33 +0200

    Create matches table

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,15 @@ import PublicRoutes from './Components/authRoutes/publicRoutes'
 import Home from './Components/Home';
 import SignIn from './Components/Signin';
 import Dashboard from './Components/Admin/Dashboard';
+import AdminMatches from './Components/Admin/Matches'
 
 const Routes = (props) => {
   return (
    <Layout>
      <Switch>
+        <PrivateRoutes {...props} path="/admin_matches" exact component={AdminMatches}/>
         <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard}/>
+        
         <PublicRoutes {...props} restricted={true} path="/sign_in" exact component={SignIn} />
         <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
      </Switch>
